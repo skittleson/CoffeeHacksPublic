648d60bcb57f8bf0a28a2b56ab36732880aa2afb
Spencer Kittleson <spencerkittleson@hotmail.com>
2020-01-18 17:25:15 -0800

    updated product template

--- src/javascript/site.js
+++ src/javascript/site.js
@@ -80,3 +80,15 @@ function CheckBrowser() {
     return false;
   }
 }
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
