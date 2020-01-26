b219f5bd94ab2b7d715d61d25310f2cb54b17a9e
Spencer Kittleson <spencerkittleson@hotmail.com>
2020-01-26 13:31:33 -0800

    home page updates

--- src/javascript/site.js
+++ src/javascript/site.js
@@ -80,15 +80,3 @@ function CheckBrowser() {
     return false;
   }
 }
-
-
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
